1207d2c98b9ffcf369c044416290e60844531210
Antonio Osorio <antonio.f.osorio@gmail.com>
2017-08-26 00:30:43 +0200

    Partial implementation of the getInstance method

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/view/EcsInstanceProvider.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/view/EcsInstanceProvider.java
@@ -1,5 +1,11 @@
 package com.netflix.spinnaker.clouddriver.ecs.view;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.ecs.AmazonECS;
+import com.amazonaws.services.ecs.model.DescribeTasksRequest;
+import com.amazonaws.services.ecs.model.ListClustersResult;
+import com.amazonaws.services.ecs.model.Task;
+import com.amazonaws.services.ecs.model.ListClustersRequest;
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
 import com.netflix.spinnaker.clouddriver.ecs.EcsCloudProvider;
@@ -7,8 +13,13 @@ import com.netflix.spinnaker.clouddriver.ecs.model.EcsInstance;
 import com.netflix.spinnaker.clouddriver.model.InstanceProvider;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentials;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider;
+import com.netflix.spinnaker.kork.web.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Component
 public class EcsInstanceProvider implements InstanceProvider<EcsInstance> {
@@ -27,14 +38,70 @@ public class EcsInstanceProvider implements InstanceProvider<EcsInstance> {
     return cloudProvider;
   }
 
+  private NetflixAmazonCredentials getCredentials(String account) {
+    NetflixAmazonCredentials accountCredentials = null;
+    for (AccountCredentials credentials: accountCredentialsProvider.getAll()) {
+      if (credentials.getName().equals(account)) {
+        if (credentials instanceof NetflixAmazonCredentials) {
+          accountCredentials = (NetflixAmazonCredentials) credentials;
+          break;
+        }
+      }
+    }
+
+    if (accountCredentials == null) {
+      throw new NotFoundException(
+        String.format("AWS account %s was not found.  Please specify a valid account name", account)
+      );
+    }
+
+    return accountCredentials;
+  }
+
   @Override
   public EcsInstance getInstance(String account, String region, String id) {
-    AccountCredentials credentials = accountCredentialsProvider.getCredentials(account);
-    if (!(credentials instanceof NetflixAmazonCredentials)) {
-      throw new IllegalArgumentException("Invalid credentials: " + account + ":" + region);
+    if (!is_valid_task_id(id)) {
+      return null;
     }
-    amazonClientProvider.getAmazonECS(account, credentials, region)
-    amazonClientProvider.getAmazonEC2((NetflixAmazonCredentials) credentials, region, true)
+
+    AWSCredentialsProvider awsCredentialsProvider = getCredentials(account).getCredentialsProvider();
+    AmazonECS amazonECS = amazonClientProvider.getAmazonECS(account, awsCredentialsProvider, region);
+
+    ListClustersResult listClustersResult = amazonECS.listClusters();
+    List<String> clusterList = listClustersResult.getClusterArns();
+    while (listClustersResult.getNextToken() != null) {
+      listClustersResult = amazonECS.listClusters(
+        new ListClustersRequest().withNextToken(listClustersResult.getNextToken())
+      );
+      clusterList.addAll(listClustersResult.getClusterArns());
+    }
+
+    EcsInstance instance = null;
+
+    List<String> queryTaskList = new ArrayList<>();
+    queryTaskList.add(id);
+
+    for (String cluster: clusterList) {
+      List<Task> responseTaskList = amazonECS.describeTasks(
+          new DescribeTasksRequest().withCluster(cluster).withTasks(queryTaskList))
+        .getTasks();
+      if (responseTaskList.size() == 1) {
+        instance = new EcsInstance();
+        break;
+      }
+    }
+
+    return instance;
+  }
+
+  /**
+   * Returns a Boolean that validates that the id parameter is a valid ECS Task ID
+   *
+   * @param  id  Task ID to validate
+   * @return     Whether the Task ID is valid or not
+   */
+  private boolean is_valid_task_id(String id) {
+    return true;
   }
 
   @Override
