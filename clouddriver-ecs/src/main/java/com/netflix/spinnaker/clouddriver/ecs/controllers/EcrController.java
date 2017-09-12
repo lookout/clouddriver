b727721abb52b18a5468b7648eb89104187d7d25
Bruno Carrier <bruno.carrier@lookout.com>
2017-09-12 19:25:22 -0400

    Added controller to allow a find image by tag stage

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/controllers/EcrController.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/controllers/EcrController.java
@@ -1,7 +1,28 @@
-package com.netflix.spinnaker.clouddriver.ecs.provider.view;
+/*
+ * Copyright 2017 Lookout, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.clouddriver.ecs.controllers;
 
 import com.amazonaws.services.ecr.AmazonECR;
-import com.amazonaws.services.ecr.model.*;
+import com.amazonaws.services.ecr.model.DescribeImagesRequest;
+import com.amazonaws.services.ecr.model.DescribeImagesResult;
+import com.amazonaws.services.ecr.model.DescribeRepositoriesRequest;
+import com.amazonaws.services.ecr.model.DescribeRepositoriesResult;
+import com.amazonaws.services.ecr.model.ImageDetail;
+import com.amazonaws.services.ecr.model.Repository;
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentials;
@@ -52,8 +73,8 @@ public class EcrController {
 
   @RequestMapping(value = "/{repository}/image", method = RequestMethod.GET)
   public List<ImageDetail> findAllImages(@PathVariable("account") String account,
-                                            @PathVariable("region") String region,
-                                            @PathVariable("repository") String repositoryName) {
+                                         @PathVariable("region") String region,
+                                         @PathVariable("repository") String repositoryName) {
     AmazonECR amazonECR = amazonClientProvider.getAmazonEcr(account, getCredentials(account).getCredentialsProvider(), region);
 
     DescribeImagesRequest request = new DescribeImagesRequest();
