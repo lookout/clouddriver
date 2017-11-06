b5e4e8343925284097f56d806f0059b8c847acc2
dkirillov <dkirillov25@gmail.com>
2017-11-06 16:18:35 -0500

    Implmented DisableServiceAtomicOperationValidator.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/validators/DisableServiceAtomicOperationValidator.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/validators/DisableServiceAtomicOperationValidator.java
@@ -16,22 +16,32 @@
 
 package com.netflix.spinnaker.clouddriver.ecs.deploy.validators;
 
+import com.netflix.spinnaker.clouddriver.aws.security.AmazonCredentials;
 import com.netflix.spinnaker.clouddriver.deploy.DescriptionValidator;
 import com.netflix.spinnaker.clouddriver.ecs.EcsOperation;
+import com.netflix.spinnaker.clouddriver.ecs.deploy.description.DisableServiceDescription;
 import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @EcsOperation(AtomicOperations.DISABLE_SERVER_GROUP)
 @Component("disableServiceAtomicOperationValidator")
-public class DisableServiceAtomicOperationValidator extends DescriptionValidator {
+public class DisableServiceAtomicOperationValidator extends CommonValidator {
 
   @Override
   public void validate(List priorDescriptions, Object description, Errors errors) {
+    DisableServiceDescription disableServiceDescription = (DisableServiceDescription) description;
 
-    // TODO - Implement this stub
+    if (disableServiceDescription.getServerGroupName() == null) {
+      errors.rejectValue("serverGroupName", "disableServiceDescription.serverGroupName.not.nullable");
+    }
 
+    validateRegions(disableServiceDescription, Collections.singleton(disableServiceDescription.getRegion()), "disableServiceDescription", errors, "region");
   }
 }
