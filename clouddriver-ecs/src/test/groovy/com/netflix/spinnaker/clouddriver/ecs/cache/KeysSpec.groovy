455b83435f962b0350f8973b1117fc7e3a176dd4
dkirillov <dkirillov25@gmail.com>
2017-11-09 10:33:58 -0500

    More tests.

--- clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/cache/KeysSpec.groovy
+++ clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/cache/KeysSpec.groovy
@@ -16,7 +16,6 @@
 
 package com.netflix.spinnaker.clouddriver.ecs.cache
 
-import spock.lang.Shared
 import spock.lang.Specification
 
 import static com.netflix.spinnaker.clouddriver.ecs.EcsCloudProvider.ID
@@ -24,32 +23,56 @@ import static com.netflix.spinnaker.clouddriver.ecs.cache.Keys.Namespace.*
 import static com.netflix.spinnaker.clouddriver.ecs.cache.Keys.SEPARATOR
 
 class KeysSpec extends Specification {
-  @Shared
-  String account = 'test-account'
-  @Shared
-  String region = 'us-east-1'
+
+  def buildKey(String namespace, String account, String region, String identifier) {
+    return ID + SEPARATOR + namespace + SEPARATOR + account + SEPARATOR + region + SEPARATOR + identifier
+  }
 
   def 'should parse a given key properly'() {
     given:
     Keys keys = new Keys()
 
     expect:
-    keys.parseKey(ID + SEPARATOR + namespace + SEPARATOR + account + SEPARATOR + region + SEPARATOR + identifier) == parsedKey
+    keys.parseKey(buildKey(namespace, account, region, identifier as String)) == parsedKey
 
     where:
 
-    namespace              | identifier                                                                                        | parsedKey
-    TASKS.ns               | '1dc5c17a-422b-4dc4-b493-371970c6c4d6'                                                            | [provider: ID, type: TASKS.ns, account: account, region: region, taskId: identifier]
-    SERVICES.ns            | 'test-stack-detail-v001'                                                                          | [provider: ID, type: SERVICES.ns, account: account, region: region, taskId: identifier]
-    ECS_CLUSTERS.ns        | 'test-cluster-1'                                                                                  | [provider: ID, type: ECS_CLUSTERS.ns, account: account, region: region, taskId: identifier]
-    CONTAINER_INSTANCES.ns | 'arn:aws:ecs:' + region + ':012345678910:container-instance/14e8cce9-0b16-4af4-bfac-a85f7587aa98' | [provider: ID, type: CONTAINER_INSTANCES.ns, account: account, region: region, taskId: identifier]
-    TASK_DEFINITIONS.ns    | 'arn:aws:ecs:' + region + ':012345678910:task-definition/hello_world:10'                          | [provider: ID, type: TASK_DEFINITIONS.ns, account: account, region: region, taskId: identifier]
+    account          | region      | namespace              | identifier                                                                                        | parsedKey
+    'test-account-1' | 'us-west-1' | TASKS.ns               | '1dc5c17a-422b-4dc4-b493-371970c6c4d6'                                                            | [provider: ID, type: TASKS.ns, account: account, region: region, taskId: identifier]
+    'test-account-2' | 'us-west-2' | SERVICES.ns            | 'test-stack-detail-v001'                                                                          | [provider: ID, type: SERVICES.ns, account: account, region: region, serviceName: identifier]
+    'test-account-3' | 'us-west-3' | ECS_CLUSTERS.ns        | 'test-cluster-1'                                                                                  | [provider: ID, type: ECS_CLUSTERS.ns, account: account, region: region, clusterName: identifier]
+    'test-account-4' | 'us-west-4' | CONTAINER_INSTANCES.ns | 'arn:aws:ecs:' + region + ':012345678910:container-instance/14e8cce9-0b16-4af4-bfac-a85f7587aa98' | [provider: ID, type: CONTAINER_INSTANCES.ns, account: account, region: region, containerInstanceArn: identifier]
+    'test-account-5' | 'us-west-5' | TASK_DEFINITIONS.ns    | 'arn:aws:ecs:' + region + ':012345678910:task-definition/hello_world:10'                          | [provider: ID, type: TASK_DEFINITIONS.ns, account: account, region: region, taskDefinitionArn: identifier]
 
   }
 
-  def 'should generate the proper task key'(){
-    //expect:
-    //Keys.getTaskKey() ==
+  def 'should generate the proper task key'() {
+    expect:
+    Keys.getTaskKey(account, region, taskId) == key
 
+    where:
+    region      | account          | taskId                                 | key
+    'us-west-1' | 'test-account-1' | '1dc5c17a-422b-4dc4-b493-371970c6c4d6' | buildKey(TASKS.ns, account, region, taskId)
+    'us-west-2' | 'test-account-2' | 'deadbeef-422b-4dc4-b493-371970c6c4d6' | buildKey(TASKS.ns, account, region, taskId)
+  }
+
+  def 'should generate the proper service key'() {
+    expect:
+    Keys.getServiceKey(account, region, serviceName) == key
+
+    where:
+    region      | account          | serviceName                            | key
+    'us-west-1' | 'test-account-1' | '1dc5c17a-422b-4dc4-b493-371970c6c4d6' | buildKey(SERVICES.ns, account, region, serviceName)
+    'us-west-2' | 'test-account-2' | 'deadbeef-422b-4dc4-b493-371970c6c4d6' | buildKey(SERVICES.ns, account, region, serviceName)
+  }
+
+  def 'should generate the proper cluster key'() {
+    expect:
+    Keys.getClusterKey(account, region, clusterName) == key
+
+    where:
+    region      | account          | clusterName      | key
+    'us-west-1' | 'test-account-1' | 'test-cluster-1' | buildKey(ECS_CLUSTERS.ns, account, region, clusterName)
+    'us-west-2' | 'test-account-2' | 'test-cluster-2' | buildKey(ECS_CLUSTERS.ns, account, region, clusterName)
   }
 }
