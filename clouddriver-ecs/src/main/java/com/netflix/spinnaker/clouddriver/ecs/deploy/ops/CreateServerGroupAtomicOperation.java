c307ae0ed26507aab56859517e6c47f0bbe78007
dkirillov <dkirillov25@gmail.com>
2017-11-14 10:06:49 -0500

    CreateServerGroupAtomicOperation now creates a MetricAlarm when metric alarms are given.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/CreateServerGroupAtomicOperation.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/CreateServerGroupAtomicOperation.java
@@ -16,12 +16,22 @@
 
 package com.netflix.spinnaker.clouddriver.ecs.deploy.ops;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.applicationautoscaling.AWSApplicationAutoScaling;
+import com.amazonaws.services.applicationautoscaling.model.AdjustmentType;
+import com.amazonaws.services.applicationautoscaling.model.PolicyType;
+import com.amazonaws.services.applicationautoscaling.model.PutScalingPolicyRequest;
+import com.amazonaws.services.applicationautoscaling.model.PutScalingPolicyResult;
 import com.amazonaws.services.applicationautoscaling.model.RegisterScalableTargetRequest;
 import com.amazonaws.services.applicationautoscaling.model.ScalableDimension;
 import com.amazonaws.services.applicationautoscaling.model.ServiceNamespace;
+import com.amazonaws.services.applicationautoscaling.model.StepAdjustment;
+import com.amazonaws.services.applicationautoscaling.model.StepScalingPolicyConfiguration;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.model.DescribeAlarmsRequest;
+import com.amazonaws.services.cloudwatch.model.DescribeAlarmsResult;
+import com.amazonaws.services.cloudwatch.model.MetricAlarm;
+import com.amazonaws.services.cloudwatch.model.PutMetricAlarmRequest;
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.model.ContainerDefinition;
 import com.amazonaws.services.ecs.model.CreateServiceRequest;
@@ -55,6 +65,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CreateServerGroupAtomicOperation implements AtomicOperation<DeploymentResult> {
   private static final String BASE_PHASE = "CREATE_ECS_SERVER_GROUP";
@@ -84,6 +95,7 @@ public class CreateServerGroupAtomicOperation implements AtomicOperation<Deploym
     String credentialAccount = description.getCredentialAccount();
     AmazonCredentials credentials = getCredentials();
     AWSApplicationAutoScaling autoScalingClient = getAmazonApplicationAutoScalingClient();
+    AmazonCloudWatch cloudWatch = getAmazonCloudWatchClient();
 
 
     AmazonECS ecs = getAmazonEcsClient(region, credentialAccount);
@@ -95,9 +107,78 @@ public class CreateServerGroupAtomicOperation implements AtomicOperation<Deploym
     String ecsServiceRole = inferAssumedRoleArn(credentials);
     Service service = createService(ecs, taskDefinition, ecsServiceRole);
 
-    createAutoScalingGroup(autoScalingClient, credentials, service);
+    String resourceId = createAutoScalingGroup(autoScalingClient, credentials, service);
+    associateAsgWithMetrics(cloudWatch, autoScalingClient, service.getServiceName(), resourceId);
 
     return makeDeploymentResult(service);
+  }
+
+  private void associateAsgWithMetrics(AmazonCloudWatch cloudWatch,
+                                       AWSApplicationAutoScaling autoScalingClient,
+                                       String serviceName,
+                                       String resourceId) {
+    //TODO: Potentially cache all/whole MetricAlarms.
+    DescribeAlarmsRequest describeAlarmsRequest = new DescribeAlarmsRequest()
+      .withAlarmNames(description.getAutoscalingPolicies().stream()
+        .map(MetricAlarm::getAlarmName)
+        .collect(Collectors.toList()));
+    DescribeAlarmsResult describeAlarmsResult = cloudWatch.describeAlarms(describeAlarmsRequest);
+
+    for (MetricAlarm metricAlarm : describeAlarmsResult.getMetricAlarms()) {
+      String policyName = serviceName + "-scaling-policy-" + metricAlarm.getAlarmName();
+      String policyArn = putScalingPolicy(autoScalingClient, resourceId, policyName);
+      cloudWatch.putMetricAlarm(buildPutMetricAlarmRequest(metricAlarm, policyArn));
+    }
+  }
+
+  private String putScalingPolicy(AWSApplicationAutoScaling autoScalingClient,
+                                  String resourceId,
+                                  String policyName) {
+
+    StepAdjustment stepAdjustment1 = new StepAdjustment()
+      .withScalingAdjustment(1)
+      .withMetricIntervalUpperBound(0.0d);
+
+    StepScalingPolicyConfiguration stepScalingPolicyConfiguration = new StepScalingPolicyConfiguration()
+      .withAdjustmentType(AdjustmentType.ChangeInCapacity)
+      .withCooldown(60)
+      .withStepAdjustments(stepAdjustment1);
+
+    PutScalingPolicyRequest putScalingPolicyRequest = new PutScalingPolicyRequest()
+      .withResourceId(resourceId)
+      .withPolicyName(policyName)
+      .withScalableDimension(ScalableDimension.EcsServiceDesiredCount)
+      .withServiceNamespace(ServiceNamespace.Ecs)
+      .withPolicyType(PolicyType.StepScaling)
+      .withStepScalingPolicyConfiguration(stepScalingPolicyConfiguration);
+
+    updateTaskStatus("Putting Scaling Policy with the name " + policyName);
+    PutScalingPolicyResult result = autoScalingClient.putScalingPolicy(putScalingPolicyRequest);
+    updateTaskStatus("Done putting Scaling Policy with the name " + policyName);
+    return result.getPolicyARN();
+  }
+
+  private PutMetricAlarmRequest buildPutMetricAlarmRequest(MetricAlarm metricAlarm, String policyArn) {
+    return new PutMetricAlarmRequest()
+      //add +"-new" or w/e to make new MetricAlarms
+      .withAlarmName(metricAlarm.getAlarmName())
+      .withEvaluationPeriods(metricAlarm.getEvaluationPeriods())
+      .withThreshold(metricAlarm.getThreshold())
+      .withActionsEnabled(metricAlarm.getActionsEnabled())
+      .withAlarmDescription(metricAlarm.getAlarmDescription())
+      .withComparisonOperator(metricAlarm.getComparisonOperator())
+      .withDimensions(metricAlarm.getDimensions())
+      .withMetricName(metricAlarm.getMetricName())
+      .withUnit(metricAlarm.getUnit())
+      .withOKActions(metricAlarm.getOKActions())
+      .withPeriod(metricAlarm.getPeriod())
+      .withNamespace(metricAlarm.getNamespace())
+      .withStatistic(metricAlarm.getStatistic())
+      .withEvaluateLowSampleCountPercentile(metricAlarm.getEvaluateLowSampleCountPercentile())
+      .withInsufficientDataActions(metricAlarm.getInsufficientDataActions())
+      .withTreatMissingData(metricAlarm.getTreatMissingData())
+      .withExtendedStatistic(metricAlarm.getExtendedStatistic())
+      .withAlarmActions(policyArn);
   }
 
   private TaskDefinition registerTaskDefinition(AmazonECS ecs) {
@@ -169,9 +250,9 @@ public class CreateServerGroupAtomicOperation implements AtomicOperation<Deploym
     return service;
   }
 
-  private void createAutoScalingGroup(AWSApplicationAutoScaling autoScalingClient,
-                                      AmazonCredentials credentials,
-                                      Service service) {
+  private String createAutoScalingGroup(AWSApplicationAutoScaling autoScalingClient,
+                                        AmazonCredentials credentials,
+                                        Service service) {
     String assumedRoleArn = inferAssumedRoleArn(credentials);
 
     RegisterScalableTargetRequest request = new RegisterScalableTargetRequest()
@@ -185,6 +266,8 @@ public class CreateServerGroupAtomicOperation implements AtomicOperation<Deploym
     updateTaskStatus("Creating Amazon Application Auto Scaling Scalable Target Definition...");
     autoScalingClient.registerScalableTarget(request);
     updateTaskStatus("Done creating Amazon Application Auto Scaling Scalable Target Definition.");
+
+    return request.getResourceId();
   }
 
   private String inferAssumedRoleArn(AmazonCredentials credentials) {
@@ -244,6 +327,14 @@ public class CreateServerGroupAtomicOperation implements AtomicOperation<Deploym
     return amazonClientProvider.getAmazonApplicationAutoScaling(credentialAccount, credentialsProvider, region);
   }
 
+  private AmazonCloudWatch getAmazonCloudWatchClient() {
+    AWSCredentialsProvider credentialsProvider = getCredentials().getCredentialsProvider();
+    String region = getRegion();
+    String credentialAccount = description.getCredentialAccount();
+
+    return amazonClientProvider.getAmazonCloudWatch(credentialAccount, credentialsProvider, region);
+  }
+
   private AmazonECS getAmazonEcsClient(String region, String account) {
     AWSCredentialsProvider credentialsProvider = getCredentials().getCredentialsProvider();
 
@@ -287,7 +378,7 @@ public class CreateServerGroupAtomicOperation implements AtomicOperation<Deploym
 
   private String inferNextServerGroupVersion() {
     int version = containerInformationService.getLatestServiceVersion(description.getEcsClusterName(), getFamilyName(), description.getCredentialAccount(), description.getRegion());
-    return String.format("v%04d", (version+ 1));
+    return String.format("v%04d", (version + 1));
   }
 
   private String getFamilyName() {
