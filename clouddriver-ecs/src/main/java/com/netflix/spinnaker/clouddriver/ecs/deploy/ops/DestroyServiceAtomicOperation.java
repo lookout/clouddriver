10f21e02f29109cb6177440fb3d2df4fa63154a3
dkirillov <dkirillov25@gmail.com>
2017-12-04 10:54:19 -0500

    Cheese removed: DestroyServiceAtomicOperation doesn't hardcode the cluster, uses ContainerInformationService instead.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DestroyServiceAtomicOperation.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DestroyServiceAtomicOperation.java
@@ -35,6 +35,7 @@ import com.netflix.spinnaker.clouddriver.data.task.TaskRepository;
 import com.netflix.spinnaker.clouddriver.ecs.cache.client.EcsCloudWatchAlarmCacheClient;
 import com.netflix.spinnaker.clouddriver.ecs.cache.model.EcsMetricAlarm;
 import com.netflix.spinnaker.clouddriver.ecs.deploy.description.DestroyServiceDescription;
+import com.netflix.spinnaker.clouddriver.ecs.services.ContainerInformationService;
 import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperation;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider;
 import org.apache.commons.lang3.StringUtils;
@@ -51,15 +52,14 @@ import java.util.stream.Collectors;
 public class DestroyServiceAtomicOperation implements AtomicOperation<Void> {
   private static final String BASE_PHASE = "DESTROY_ECS_SERVER_GROUP";
 
-  // TODO: Remove hardcoded CLUSTER_NAME.
-  private static final String CLUSTER_NAME = "poc";
-
   @Autowired
   AmazonClientProvider amazonClientProvider;
   @Autowired
   AccountCredentialsProvider accountCredentialsProvider;
   @Autowired
   EcsCloudWatchAlarmCacheClient metricAlarmCacheClient;
+  @Autowired
+  ContainerInformationService containerInformationService;
 
   DestroyServiceDescription description;
 
@@ -77,6 +77,8 @@ public class DestroyServiceAtomicOperation implements AtomicOperation<Void> {
     AmazonCredentials credentials = (AmazonCredentials) accountCredentialsProvider.getCredentials(description.getCredentialAccount());
     AmazonECS ecs = amazonClientProvider.getAmazonEcs(description.getCredentialAccount(), credentials.getCredentialsProvider(), description.getRegion());
 
+    String clusterName = containerInformationService.getClusterName(description.getServerGroupName(), description.getAccount(), description.getRegion());
+
     getTask().updateStatus(BASE_PHASE, "Removing MetricAlarms from " + description.getServerGroupName() + ".");
     deleteMetrics();
     getTask().updateStatus(BASE_PHASE, "Done removing MetricAlarms from " + description.getServerGroupName() + ".");
@@ -84,14 +86,14 @@ public class DestroyServiceAtomicOperation implements AtomicOperation<Void> {
     UpdateServiceRequest updateServiceRequest = new UpdateServiceRequest();
     updateServiceRequest.setService(description.getServerGroupName());
     updateServiceRequest.setDesiredCount(0);
-    updateServiceRequest.setCluster(CLUSTER_NAME);
+    updateServiceRequest.setCluster(clusterName);
 
     getTask().updateStatus(BASE_PHASE, "Scaling " + description.getServerGroupName() + " service down to 0.");
     ecs.updateService(updateServiceRequest);
 
     DeleteServiceRequest deleteServiceRequest = new DeleteServiceRequest();
     deleteServiceRequest.setService(description.getServerGroupName());
-    deleteServiceRequest.setCluster(CLUSTER_NAME);
+    deleteServiceRequest.setCluster(clusterName);
 
     getTask().updateStatus(BASE_PHASE, "Deleting " + description.getServerGroupName() + " service.");
     DeleteServiceResult deleteServiceResult = ecs.deleteService(deleteServiceRequest);
