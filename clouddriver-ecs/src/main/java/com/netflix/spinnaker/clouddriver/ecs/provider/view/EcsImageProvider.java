f150e84192ced63dd1f15a65c5d99634c2cde66c
dkirillov <dkirillov25@gmail.com>
2017-12-11 15:53:34 -0500

    Added missing files.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/provider/view/EcsImageProvider.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/provider/view/EcsImageProvider.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.netflix.spinnaker.clouddriver.ecs.controllers;
+package com.netflix.spinnaker.clouddriver.ecs.provider.view;
 
 import com.amazonaws.services.ecr.AmazonECR;
 import com.amazonaws.services.ecr.model.DescribeImagesRequest;
@@ -23,20 +23,16 @@ import com.amazonaws.services.ecr.model.ImageDetail;
 import com.amazonaws.services.ecr.model.ListImagesRequest;
 import com.amazonaws.services.ecr.model.ListImagesResult;
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider;
-import com.netflix.spinnaker.clouddriver.aws.security.AmazonCredentials.AWSRegion;
+import com.netflix.spinnaker.clouddriver.aws.security.AmazonCredentials;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentials;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider;
 import com.netflix.spinnaker.kork.web.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,27 +40,99 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/ecs")
-public class EcsFindImagesByTagController {
+@Component
+public class EcsImageProvider implements ImageRepositoryProvider {
   private static final Pattern ACCOUNT_ID_PATTERN = Pattern.compile("^([0-9]{12})");
   private static final Pattern REPOSITORY_NAME_PATTERN = Pattern.compile("\\/([a-z0-9._-]+)");
   private static final String IDENTIFIER_PATTERN = "(:([a-z0-9._-]+)|@(sha256:[0-9a-f]{64}))";
   private static final Pattern REGION_PATTERN = Pattern.compile("(\\w+-\\w+-\\d+)");
-  private static final Pattern REPOSITORY_URI_PATTERN = Pattern.compile(ACCOUNT_ID_PATTERN.toString() + ".+" +
+  private static final Pattern REPOSITORY_URI_PATTERN = Pattern.compile(ACCOUNT_ID_PATTERN.toString() + "\\.dkr\\.ecr\\." +
     REGION_PATTERN.toString() + ".+" +
     REPOSITORY_NAME_PATTERN.toString() +
     IDENTIFIER_PATTERN);
 
-  AmazonClientProvider amazonClientProvider;
+  private final AmazonClientProvider amazonClientProvider;
 
-  AccountCredentialsProvider accountCredentialsProvider;
+  private final AccountCredentialsProvider accountCredentialsProvider;
 
   @Autowired
-  public EcsFindImagesByTagController(AmazonClientProvider amazonClientProvider,
-                                      AccountCredentialsProvider accountCredentialsProvider) {
+  public EcsImageProvider(AmazonClientProvider amazonClientProvider,
+                          AccountCredentialsProvider accountCredentialsProvider) {
     this.amazonClientProvider = amazonClientProvider;
     this.accountCredentialsProvider = accountCredentialsProvider;
+  }
+
+  @Override
+  public String getRepositoryName() {
+    return "ECR";
+  }
+
+  @Override
+  public boolean handles(String url) {
+    return url.contains(".dkr.ecr.");
+  }
+
+  @Override
+  public Object findImage(String url) {
+    // HTTP(S) part is not needed.
+    url = url.replace("http://", "").replace("https://", "");
+
+    String accountId = extractAwsAccountId(url);
+    String repository = extractEcrRepositoryName(url);
+    String identifier = extractEcrIdentifier(repository, url);
+    boolean isTag = !(identifier.startsWith("sha256:") && identifier.length() == ("sha256:".length() + 64));
+    String region = extractAwsRegion(url);
+
+    NetflixAmazonCredentials credentials = getCredentials(accountId);
+
+    if (!isValidRegion(credentials, region)) {
+      throw new Error("The repository URI provided does not belong to a region that the credentials have access to or is not a valid region.");
+    }
+
+    if (!isValidEcrUrl(url)) {
+      throw new Error("The repository URI provided is not properly structured.");
+    }
+
+    AmazonECR amazonECR = amazonClientProvider.getAmazonEcr(credentials.getName(), credentials.getCredentialsProvider(), region);
+
+    ListImagesResult result = amazonECR.listImages(new ListImagesRequest().withRegistryId(accountId).withRepositoryName(repository));
+    DescribeImagesResult imagesResult = amazonECR.describeImages(new DescribeImagesRequest().withRegistryId(accountId).withRepositoryName(repository).withImageIds(result.getImageIds()));
+
+    // TODO - what is the user interface we want to have here?  We should discuss with Lars and Ethan from the community as this whole thing will undergo a big refactoring
+    List<ImageDetail> imagesWithThisIdentifier = imagesResult.getImageDetails().stream()
+      .filter(imageDetail -> isTag ?
+        imageDetail.getImageTags() != null && imageDetail.getImageTags().contains(identifier) :
+        imageDetail.getImageDigest().equals(identifier))
+      .collect(Collectors.toList());
+
+    if (imagesWithThisIdentifier.size() > 1) {
+      throw new Error("More than 1 image has this " + (isTag ? "tag" : "digest") + "!  We can't handle this in the POC!");
+    } else if (imagesWithThisIdentifier.size() == 0) {
+      throw new Error(String.format("No image with the " + (isTag ? "tag" : "digest") + " %s was found.", identifier));
+    }
+
+    ImageDetail matchedImage = imagesWithThisIdentifier.get(0);
+
+    Map<String, Object> map = new HashMap<>();
+    map.put("region", region);
+
+    map.put("imageName", buildFullDockerImageUrl(matchedImage.getImageDigest(),
+      matchedImage.getRegistryId(),
+      matchedImage.getRepositoryName(),
+      region));
+
+    Map<String, List<String>> amis = new HashMap<>();
+    amis.put(region, Collections.singletonList(matchedImage.getImageDigest()));
+    map.put("amis", amis);
+
+    Map<String, Object> attributes = new HashMap<>();
+    attributes.put("creationDate", matchedImage.getImagePushedAt());
+    map.put("attributes", attributes);
+
+    List<Map<String, Object>> responseBody = new ArrayList<>();
+    responseBody.add(map);
+
+    return responseBody;
   }
 
   private NetflixAmazonCredentials getCredentials(String accountId) {
@@ -76,117 +144,56 @@ public class EcsFindImagesByTagController {
         }
       }
     }
-
-
-    throw new NotFoundException(String.format("AWS account %s was not found.  Please specify a valid account name"));
+    throw new NotFoundException(String.format("AWS account %s was not found.  Please specify a valid account name", accountId));
   }
 
-  @RequestMapping(value = "/images/find", method = RequestMethod.GET)
-  public Object findImage(@RequestParam("q") String dockerImageUrl, HttpServletRequest request) {
-    // TODO: Make this method repo-generic, to allow support for say DockerHub. Have a method findEcrImage that contains the code below, and a method such as findDockerHubImage for when DockerHub repo is supported.
-
-    // HTTP(S) part is not needed.
-    dockerImageUrl = dockerImageUrl.replace("http://", "").replace("https://", "");
-
-    if (!dockerImageUrl.contains(".ecr.")) {
-      throw new Error("The repository URI provided is not an ECR URI. Currently only ECR URIs are supported.");
-    }
-
-    Matcher matcher = ACCOUNT_ID_PATTERN.matcher(dockerImageUrl);
-    if (!matcher.find()) {
-      throw new Error("The repository URI provided does not contain a proper account ID.");
-    }
-    String accountId = matcher.group(1);
-
-    matcher = REPOSITORY_NAME_PATTERN.matcher(dockerImageUrl);
-    if (!matcher.find()) {
-      throw new Error("The repository URI provided does not contain a proper repository name.");
-    }
-    String repository = matcher.group(1);
-
-    final Pattern identifierPatter = Pattern.compile(repository + IDENTIFIER_PATTERN);
-    matcher = identifierPatter.matcher(dockerImageUrl);
-    if (!matcher.find()) {
-      throw new Error("The repository URI provided does not contain a proper tag or sha256 digest.");
-    }
-    final boolean isTag = matcher.group(1).startsWith(":");
-    final String identifier = isTag ? matcher.group(2) : matcher.group(3);
-
-    matcher = REPOSITORY_URI_PATTERN.matcher(dockerImageUrl);
-    if (!matcher.find()) {
-      throw new Error("The repository URI provided is not properly structured.");
-    }
-
-    matcher = REGION_PATTERN.matcher(dockerImageUrl);
-    if (!matcher.find()) {
-      throw new Error("The repository URI provided does not contain a proper region.");
-    }
-    String region = matcher.group();
-
-    NetflixAmazonCredentials credentials = getCredentials(accountId);
-
-    if (!isValidRegion(credentials, region)) {
-      throw new Error("The repository URI provided does not belong to a region that the credentials have access to or is not a valid region.");
-    }
-
-    AmazonECR amazonECR = amazonClientProvider.getAmazonEcr(credentials.getName(), credentials.getCredentialsProvider(), region);
-
-    ListImagesResult result = amazonECR.listImages(new ListImagesRequest().withRegistryId(accountId).withRepositoryName(repository));
-    DescribeImagesResult imagesResult = amazonECR.describeImages(new DescribeImagesRequest().withRegistryId(accountId).withRepositoryName(repository).withImageIds(result.getImageIds()));
-
-    List<ImageDetail> imagesWithThisIdentifier = imagesResult.getImageDetails().stream()
-      .filter(imageDetail -> isTag ? imageDetail.getImageTags() != null && imageDetail.getImageTags().contains(identifier) : // TODO - what is the user interface we want to have here?  We should discuss with Lars and Ethan from the community as this whole thing will undergo a big refactoring
-        imageDetail.getImageDigest().equals(identifier))
-      .collect(Collectors.toList());
-
-    if (imagesWithThisIdentifier.size() > 1) {
-      throw new Error("More than 1 image has this " + (isTag ? "tag" : "digest") + "!  We can't handle this in the POC!");
-    } else if (imagesWithThisIdentifier.size() == 0) {
-      throw new Error(String.format("No image with the " + (isTag ? "tag" : "digest") + " %s was found.", identifier));
-    }
-
-
-    ImageDetail matchedImage = imagesWithThisIdentifier.get(0);
-
-    List<Map<String, Object>> responseBody = new ArrayList<>();
-
-    Map<String, Object> map = new HashMap<>();
-    map.put("region", region);
-
-    map.put("imageName", buildFullDockerImageUrl(matchedImage.getImageDigest(),
-      matchedImage.getRegistryId(),
-      matchedImage.getRepositoryName(),
-      region));
-
-    Map<String, List<String>> amis = new HashMap<>();
-    amis.put(region, Arrays.asList(matchedImage.getImageDigest()));
-    map.put("amis", amis);
-
-    Map<String, Object> attributes = new HashMap<>();
-    attributes.put("creationDate", matchedImage.getImagePushedAt());
-    map.put("attributes", attributes);
-
-    responseBody.add(map);
-
-    return responseBody;
-  }
 
   private boolean isValidRegion(NetflixAmazonCredentials credentials, String region) {
     return credentials.getRegions().stream()
-      .map(AWSRegion::getName)
+      .map(AmazonCredentials.AWSRegion::getName)
       .anyMatch(region::equals);
   }
 
+  private boolean isValidEcrUrl(String imageUrl) {
+    Matcher matcher = REPOSITORY_URI_PATTERN.matcher(imageUrl);
+    return matcher.find();
+  }
+
+  private String extractAwsAccountId(String imageUrl) {
+    return extractString(ACCOUNT_ID_PATTERN, imageUrl, 1,
+      "The repository URI provided does not contain a proper account ID.");
+  }
+
+  private String extractEcrRepositoryName(String imageUrl) {
+    return extractString(REPOSITORY_NAME_PATTERN, imageUrl, 1,
+      "The repository URI provided does not contain a proper repository name.");
+  }
+
+  private String extractAwsRegion(String imageUrl) {
+    return extractString(REGION_PATTERN, imageUrl, 0,
+      "The repository URI provided does not contain a proper region.");
+  }
+
+  private String extractString(Pattern pattern, String imageUrl, int group, String error) {
+    Matcher matcher = pattern.matcher(imageUrl);
+    if (!matcher.find()) {
+      throw new Error(error);
+    }
+    return matcher.group(group);
+  }
+
+  private String extractEcrIdentifier(String repository, String imageUrl) {
+    final Pattern identifierPatter = Pattern.compile(repository + IDENTIFIER_PATTERN);
+    Matcher matcher = identifierPatter.matcher(imageUrl);
+    if (!matcher.find()) {
+      throw new Error("The repository URI provided does not contain a proper tag or sha256 digest.");
+    }
+    return matcher.group(1).startsWith(":") ?
+      matcher.group(2) :
+      matcher.group(3);
+  }
 
   private String buildFullDockerImageUrl(String imageDigest, String registryId, String repositoryName, String region) {
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append(registryId)
-      .append(".dkr.ecr.")
-      .append(region)
-      .append(".amazonaws.com/")
-      .append(repositoryName)
-      .append("@")
-      .append(imageDigest);
-    return stringBuilder.toString();
+    return registryId + ".dkr.ecr." + region + ".amazonaws.com/" + repositoryName + "@" + imageDigest;
   }
 }
