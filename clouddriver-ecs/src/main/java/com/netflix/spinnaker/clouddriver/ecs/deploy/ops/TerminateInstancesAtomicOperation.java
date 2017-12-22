356a70f6819989f3a0e0afddbf467e13e1b890b1
Daniel Kirillov <dkirillov25@gmail.com>
2017-12-22 13:12:44 -0500

    Refactored TerminateInstancesAtomicOperation, created a validator for its' description, and wrote tests. (#89)

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/TerminateInstancesAtomicOperation.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/TerminateInstancesAtomicOperation.java
@@ -18,52 +18,23 @@ package com.netflix.spinnaker.clouddriver.ecs.deploy.ops;
 
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.model.StopTaskRequest;
-import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider;
-import com.netflix.spinnaker.clouddriver.aws.security.AmazonCredentials;
-import com.netflix.spinnaker.clouddriver.data.task.Task;
-import com.netflix.spinnaker.clouddriver.data.task.TaskRepository;
 import com.netflix.spinnaker.clouddriver.ecs.deploy.description.TerminateInstancesDescription;
-import com.netflix.spinnaker.clouddriver.ecs.services.ContainerInformationService;
-import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperation;
-import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public class TerminateInstancesAtomicOperation implements AtomicOperation<Void> {
-
-  private static final String BASE_PHASE = "TERMINATE_ECS_INSTANCES";
-
-  @Autowired
-  AmazonClientProvider amazonClientProvider;
-  @Autowired
-  AccountCredentialsProvider accountCredentialsProvider;
-  @Autowired
-  ContainerInformationService containerInformationService;
-
-  TerminateInstancesDescription description;
+public class TerminateInstancesAtomicOperation extends AbstractEcsAtomicOperation<TerminateInstancesDescription, Void> {
 
   public TerminateInstancesAtomicOperation(TerminateInstancesDescription description) {
-    this.description = description;
-  }
-
-  private static Task getTask() {
-    return TaskRepository.threadLocalTask.get();
+    super(description, "TERMINATE_ECS_INSTANCES");
   }
 
   @Override
   public Void operate(List priorOutputs) {
-    getTask().updateStatus(BASE_PHASE, "Initializing Terminate ECS Instances Operation...");
-    AmazonCredentials credentials;
-    credentials = (AmazonCredentials) accountCredentialsProvider.getCredentials(description.getCredentialAccount());
-    AmazonECS ecs = amazonClientProvider.getAmazonEcs(
-      description.getCredentialAccount(),
-      credentials.getCredentialsProvider(),
-      description.getRegion()
-    );
+    updateTaskStatus("Initializing Terminate ECS Instances Operation...");
+    AmazonECS ecs = getAmazonEcsClient();
 
-    for (String taskId: description.getEcsTaskIds()) {
-      getTask().updateStatus(BASE_PHASE, "Terminating instance: " + taskId);
+    for (String taskId : description.getEcsTaskIds()) {
+      updateTaskStatus("Terminating instance: " + taskId);
       String clusterArn = containerInformationService.getClusterArn(description.getCredentialAccount(), description.getRegion(), taskId);
       StopTaskRequest request = new StopTaskRequest().withTask(taskId).withCluster(clusterArn);
       ecs.stopTask(request);
@@ -71,4 +42,5 @@ public class TerminateInstancesAtomicOperation implements AtomicOperation<Void> 
 
     return null;
   }
+  
 }
